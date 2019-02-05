300dc3c0e212ce7df0c9787b726d033cd040504d
Ned Ruggeri <ruggeri@self-loop.com>
2019-02-05 14:09:44 -0800

    write sorting logic for mapreduce

--- src/mapreduce/common_reduce.go
+++ src/mapreduce/common_reduce.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"sort"
 )
 
 func doReduce(
@@ -14,7 +15,7 @@ func doReduce(
 	nMap int, // the number of map tasks that were run ("M" in the paper)
 	reduceF func(key string, values []string) string,
 ) {
-	// TODO: must sort the mapper output files.
+	doSort(jobName, nMap, reduceTask)
 
 	// Open the reducer's input files.
 	reducerInputManager := NewReducerInputManager(jobName, nMap, reduceTask)
@@ -67,6 +68,54 @@ func doReduce(
 		err = outputEncoder.Encode(outputKeyValue)
 		if err != nil {
 			log.Fatalf("reduce output error: %v\n", err)
+		}
+	}
+}
+
+func doSort(jobName string, numMappers int, reduceTaskIdx int) {
+	// TODO: Change this into an external merge sort.
+
+	// Open the reducer's input files.
+	reducerInputManager := NewReducerInputManager(jobName, numMappers, reduceTaskIdx)
+	defer reducerInputManager.Close()
+
+	for mapTaskIdx, inputDecoder := range reducerInputManager.inputDecoders {
+		// Read in all KeyValues for this mapper input. Gross.
+		keyValues := []KeyValue{}
+		for {
+			keyValue := &KeyValue{}
+			err := inputDecoder.Decode(keyValue)
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				log.Fatalf("unexpected decode error: %v\n", err)
+			}
+
+			keyValues = append(keyValues, *keyValue)
+		}
+
+		// Close the file.
+		reducerInputManager.inputFiles[mapTaskIdx].Close()
+
+		// Now do the sorting.
+		sort.SliceStable(keyValues, func(i, j int) bool {
+			return keyValues[i].Key < keyValues[j].Key
+		})
+
+		// Open a new file for writing. Really I should be creating a new
+		// file and then re-naming.
+		file, err := os.OpenFile(reduceName(jobName, mapTaskIdx, reduceTaskIdx), os.O_TRUNC|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatalf("unexpected error re-opening %v\n", err)
+		}
+		defer file.Close()
+
+		encoder := json.NewEncoder(file)
+		for _, keyValue := range keyValues {
+			err := encoder.Encode(keyValue)
+			if err != nil {
+				log.Fatalf("unexpected error encoding KeyValue: %v\n", err)
+			}
 		}
 	}
 }
