d0f4eb5a9adfe7b7d387c235937ceeb5ac0400b9
Ned Ruggeri <ruggeri@self-loop.com>
2019-02-07 15:40:10 -0800

    break out a sortReducerInputFile function

--- src/mapreduce/reducer/sorting.go
+++ src/mapreduce/reducer/sorting.go
@@ -10,49 +10,68 @@ import (
 )
 
 func sortReducerInputFiles(jobName string, numMappers int, reduceTaskIdx int) {
-	// TODO: Change this into an external merge sort.
-
 	// Open the reducer's input files.
 	reducerInputManager := NewInputManager(jobName, numMappers, reduceTaskIdx)
 	defer reducerInputManager.Close()
 
 	for mapTaskIdx, inputDecoder := range reducerInputManager.inputDecoders {
-		// Read in all KeyValues for this mapper input. Gross.
-		keyValues := []common.KeyValue{}
-		for {
-			keyValue := &common.KeyValue{}
-			err := inputDecoder.Decode(keyValue)
-			if err == io.EOF {
-				break
-			} else if err != nil {
-				log.Fatalf("unexpected decode error: %v\n", err)
-			}
+		// Sort each file.
+		sortReducerInputFile(
+			jobName,
+			mapTaskIdx,
+			reduceTaskIdx,
+			reducerInputManager.inputFiles[mapTaskIdx],
+			inputDecoder,
+		)
+	}
+}
 
-			keyValues = append(keyValues, *keyValue)
+func sortReducerInputFile(
+	jobName string,
+	mapTaskIdx int,
+	reduceTaskIdx int,
+	inputReadingFile os.File,
+	inputDecoder *json.Decoder) {
+	// TODO: Change this into an external merge sort.
+
+	// Read in all KeyValues for this mapper input. Gross.
+	keyValues := []common.KeyValue{}
+	for {
+		keyValue := &common.KeyValue{}
+		err := inputDecoder.Decode(keyValue)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatalf("unexpected decode error: %v\n", err)
 		}
 
-		// Close the file.
-		reducerInputManager.inputFiles[mapTaskIdx].Close()
+		keyValues = append(keyValues, *keyValue)
+	}
 
-		// Now do the sorting.
-		sort.SliceStable(keyValues, func(i, j int) bool {
-			return keyValues[i].Key < keyValues[j].Key
-		})
+	// Close the file.
+	inputReadingFile.Close()
 
-		// Open a new file for writing. Really I should be creating a new
-		// file and then re-naming.
-		file, err := os.OpenFile(common.IntermediateFileName(jobName, mapTaskIdx, reduceTaskIdx), os.O_TRUNC|os.O_WRONLY, 0644)
+	// Now do the sorting.
+	sort.SliceStable(keyValues, func(i, j int) bool {
+		return keyValues[i].Key < keyValues[j].Key
+	})
+
+	// Open a new file for writing. Really I should be creating a new
+	// file and then re-naming.
+	inputFileName := common.IntermediateFileName(
+		jobName, mapTaskIdx, reduceTaskIdx,
+	)
+	inputWritingFile, err := os.OpenFile(inputFileName, os.O_TRUNC|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("unexpected error re-opening %v\n", err)
+	}
+	defer inputWritingFile.Close()
+
+	encoder := json.NewEncoder(inputWritingFile)
+	for _, keyValue := range keyValues {
+		err := encoder.Encode(keyValue)
 		if err != nil {
-			log.Fatalf("unexpected error re-opening %v\n", err)
-		}
-		defer file.Close()
-
-		encoder := json.NewEncoder(file)
-		for _, keyValue := range keyValues {
-			err := encoder.Encode(keyValue)
-			if err != nil {
-				log.Fatalf("unexpected error encoding KeyValue: %v\n", err)
-			}
+			log.Fatalf("unexpected error encoding KeyValue: %v\n", err)
 		}
 	}
 }
