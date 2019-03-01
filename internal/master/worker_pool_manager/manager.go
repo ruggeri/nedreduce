0212a7f692e62b6f780cb50f0326cd481f8fe352
Ned Ruggeri <ruggeri@self-loop.com>
2019-03-01 13:55:31 -0800

    write WorkerAddressManager
    this will be used to pipe down connecting workers to whomever is
    interested.

---
+++ internal/master/worker_pool_manager/manager.go
@@ -0,0 +1,100 @@
+package worker_pool_manager
+
+import (
+	"sync"
+)
+
+// WorkerPoolManager can be either "running" or "shutdown"
+type managerState string
+
+const (
+	running  = managerState("Running")
+	shutdown = managerState("Shutdown")
+)
+
+// A WorkerPoolManager keeps track of those workers who register with
+// us.
+type WorkerPoolManager struct {
+	// The WorkerPoolManager keeps track of the available
+	// workerRPCAddresses.
+	workerRPCAddresses []string
+	// The state is "running" if new worker registrations are being
+	// accepted. Else it is "shutdown."
+	state managerState
+
+	// Internal threads can mutate the manager's internal state, while
+	// external threads may want to query the manager's internal state.
+	// Therefore we must synchronize mutation to the manager's internal
+	// state.
+	mutex sync.Mutex
+	// External threads can wait on an update to the WorkerPoolManager's
+	// internal state. This condition variable is used to signal them.
+	conditionVariable *sync.Cond
+}
+
+// StartManager creates a new WorkerPoolManager and starts a listener
+// running on it.
+func StartManager() *WorkerPoolManager {
+	manager := &WorkerPoolManager{
+		state:              running,
+		workerRPCAddresses: []string{},
+	}
+
+	manager.conditionVariable = sync.NewCond(&manager.mutex)
+
+	return manager
+}
+
+// SendNewWorker notifies the manager of a new worker that has
+// connected.
+func (manager *WorkerPoolManager) SendNewWorker(workerRPCAddress string) {
+	// Do everything in a goroutine so that this function won't block the
+	// caller.
+	go func() {
+		// Acquire mutex because we will update the state.
+		manager.mutex.Lock()
+		defer manager.mutex.Unlock()
+
+		// If manager is shutdown then don't bother.
+		if manager.state == shutdown {
+			return
+		}
+
+		// TODO: Should probably look for accidental reconnection
+		// (deduplicate).
+		manager.workerRPCAddresses = append(manager.workerRPCAddresses, workerRPCAddress)
+
+		// Notify and wake any listeners.
+		manager.conditionVariable.Broadcast()
+	}()
+}
+
+// SendShutdown tells a manager that there will be no more connections.
+// The internal message listener thread can be told to shut down now.
+func (manager *WorkerPoolManager) SendShutdown() {
+	// Do everything in a goroutine so that this function won't block the
+	// caller.
+	go func() {
+		// Acquire mutex because we will update the state.
+		manager.mutex.Lock()
+		defer manager.mutex.Unlock()
+
+		// If manager is shutdown then don't bother.
+		if manager.state == shutdown {
+			return
+		}
+
+		manager.state = shutdown
+
+		// Notify and wake any listeners.
+		manager.conditionVariable.Broadcast()
+	}()
+}
+
+// WorkersRPCAddressStream gives the caller a channel on which
+// connecting workers' RPC addresses are piped down.
+func (manager *WorkerPoolManager) WorkersRPCAddressStream() chan string {
+	// Check out the workerAddressStreamer to see who and how the RPC
+	// addresses are pushed on down.
+	return makeWorkersRPCAddressStream(manager)
+}
---
+++ internal/master/worker_pool_manager/worker_address_streamer.go
@@ -0,0 +1,90 @@
+package worker_pool_manager
+
+// workerAddressStreamer is a helper that streams worker RPC addresses
+// down a channel.
+type workerAddressStreamer struct {
+	manager *WorkerPoolManager
+	// Track how many worker addresses we have sent.
+	numWorkersSent            int
+	workerRPCAddressesChannel chan string
+}
+
+func makeWorkersRPCAddressStream(manager *WorkerPoolManager) chan string {
+	streamer := &workerAddressStreamer{
+		manager:                   manager,
+		numWorkersSent:            0,
+		workerRPCAddressesChannel: make(chan string),
+	}
+
+	// Start the streamer in a background goroutine.
+	go streamer.run()
+
+	// Return the channel it will pipe the worker RPC addresses down.
+	return streamer.workerRPCAddressesChannel
+}
+
+// This method is run in a background goroutine to push worker RPC
+// addresses down a channel.
+func (streamer *workerAddressStreamer) run() {
+	// Lock the manager so the state can't change on us while we check it
+	// out.
+	streamer.manager.mutex.Lock()
+	defer streamer.manager.mutex.Unlock()
+
+	for {
+		if streamer.manager.state == shutdown {
+			// When the manager is closed, all workers will be released from
+			// the pool, and therefore we shouldn't bother sending those. Let
+			// the user know we are done by closing the stream.
+			close(streamer.workerRPCAddressesChannel)
+			// The streamer has nothing left to do. It exits.
+			return
+		} else if streamer.numWorkersSent < len(streamer.manager.workerRPCAddresses) {
+			// There are new workers to stream on down. Handle them. (Note:
+			// handleNewWorkers will unlock the manager's mutex.)
+			streamer.handleNewWorkers()
+
+			// Since handleNewWorkers unlocked the mutex on the manager's
+			// internal state, the manager may have accepted even more new
+			// workers while we were doing our streaming. To check for that
+			// possibility, first relock the mutex...
+			streamer.manager.mutex.Lock()
+			// And jump to the top of the loop to double-check whether
+			// anything has happened while we were streaming.
+			continue
+		} else {
+			// Nothing has happened since last time, so there is nothing to
+			// do.
+		}
+
+		// Pause here until the manager tells us some new events have
+		// occurred.
+		//
+		// Condition variables release the mutex while waiting, so waiting
+		// here won't block the manager. On wake, the condition variable
+		// reacquires the mutex. It's important to reacquire the mutex so
+		// that we can inspect the manager's internal state once again.
+		streamer.manager.conditionVariable.Wait()
+	}
+}
+
+// handleNewWorkers streams down new worker RPC addresses to the user.
+// Warning: this function releases the lock on the manager's state!
+func (streamer *workerAddressStreamer) handleNewWorkers() {
+	// Get the new workers to send on down.
+	workersToSend := streamer.manager.workerRPCAddresses[streamer.numWorkersSent:]
+	// Be careful and make a "defensive copy" and don't try to hold on to
+	// the manager's internal state. We're about to unlock the manager.
+	workersToSend = append([]string(nil), workersToSend...)
+
+	// We will send the workers' RPC addresses down a channel, which is a
+	// *blocking* operation. We don't want to block the manager while we
+	// send them. Therefore, we unlock the mutex on the manager.
+	streamer.manager.mutex.Unlock()
+
+	// Now send each of the new workers down the channel.
+	for _, workerToSend := range workersToSend {
+		streamer.workerRPCAddressesChannel <- workerToSend
+		streamer.numWorkersSent++
+	}
+}
