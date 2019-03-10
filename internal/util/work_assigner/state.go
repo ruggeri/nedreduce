098a757a5bf4dc6d100c385fa1f4b9a2f643065a
Ned Ruggeri <ruggeri@self-loop.com>
2019-03-09 23:38:43 -0800

    strip out WorkerRegistrationManager and WorkAssigner

--- internal/util/work_assigner/state.go
+++
@@ -1,9 +0,0 @@
-package work_assigner
-
-type state string
-
-const (
-	assigningNewWork              = state("assigningNewWork")
-	waitingForLastWorksToComplete = state("waitingForLastWorksToComplete")
-	allWorkIsComplete             = state("allWorkIsComplete")
-)
