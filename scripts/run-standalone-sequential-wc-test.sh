5cf0810b55c23cc7ee2a915e090e0a35146b6108
Ned Ruggeri <ruggeri@self-loop.com>
2019-03-11 12:05:04 -0700

    start fixing sequential test to use new client/server architecture

--- scripts/run-standalone-sequential-wc-test.sh
+++ scripts/run-standalone-sequential-wc-test.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-./build/bin/wc coordinator sequential assets/pg-*.txt
+set -e
+
+# Start the coordinator
+./build/bin/wc run-coordinator coordinator &
+COORDINATOR_PID=$!
+
+# TODO: This is very lame. Basically, the coordinator is started above,
+# but we need to wait until it is actually listening for RPCs. The
+# correct way is that run-coordinator should spawn a process for running
+# the coordinator, and *return* when the process has started listening.
+sleep 1
+
+# Submit the sequential job to the coordinator. Wait for completion.
+./build/bin/wc submit-job coordinator sequential 3 assets/pg-*.txt
+
+# TODO: I should kill the coordinator via RPC.
+kill $COORDINATOR_PID
+
 sort -n -k2 mrtmp.wcseq | tail -10
 
 # TODO(LOW): Restore test so that it does a comparison to correct
