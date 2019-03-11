745fc737b85c7b3d2f7f2694a0b60a5bd7f0f35e
Ned Ruggeri <ruggeri@self-loop.com>
2019-03-11 13:21:37 -0700

    update standalone parallel script to use new client/server architecture

--- scripts/run-standalone-parallel-wc-test.sh
+++ scripts/run-standalone-parallel-wc-test.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
 
-echo "Starting coordinator."
-./build/bin/wc coordinator localhost:3000 assets/pg-*.txt &
-MASTER_PID=$!
+set -e
 
-echo "Starting worker."
-./build/bin/wc worker localhost:3000 localhost:3001 assets/pg-*.txt &
-echo "Starting worker."
-./build/bin/wc worker localhost:3000 localhost:3002 assets/pg-*.txt &
+# Start the coordinator
+./build/bin/wc run-coordinator coordinator &
+COORDINATOR_PID=$!
 
-wait $MASTER_PID
+# TODO: This is very lame. Basically, the coordinator is started above,
+# but we need to wait until it is actually listening for RPCs. The
+# correct way is that run-coordinator should spawn a process for running
+# the coordinator, and *return* when the process has started listening.
+sleep 1
+
+# Starting workers.
+./build/bin/wc run-worker coordinator worker1 assets/pg-*.txt &
+WORKER1_PID=$!
+./build/bin/wc run-worker coordinator worker2 assets/pg-*.txt &
+WORKER2_PID=$!
+./build/bin/wc run-worker coordinator worker3 assets/pg-*.txt &
+WORKER3_PID=$!
+./build/bin/wc run-worker coordinator worker4 assets/pg-*.txt &
+WORKER4_PID=$!
+
+# Submit the job to the coordinator. Wait for completion.
+./build/bin/wc submit-job coordinator distributed 3 assets/pg-*.txt
+
+# Having waited for the coordinator to finish the job, now tell it to
+# shut down.
+./build/bin/wc shutdown-coordinator coordinator
+
+# TODO: We really should kill the WORKER_PIDs properly
+echo $WORKER1_PID
+echo $WORKER2_PID
+echo $WORKER3_PID
+echo $WORKER4_PID
 
 sort -n -k2 mrtmp.wcseq | tail -10
 
